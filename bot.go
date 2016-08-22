a1be9ef51b16ba3481657951d652d75acc73cd05
junhyo <nanunsin@gmail.com>
2016-08-22 14:36:36 +0900

    modify Print Func

--- bot.go
+++ bot.go
@@ -26,8 +26,9 @@ func (bot *Mark1) PrintAll() {
 	for i := 0; i < len(bot.ObjInfo); i++ {
 		//fmt.Printf("%s\t", bot.ObjInfo[i].TimeStamp.String())
 		printTimeStamp(bot.ObjInfo[i].TimeStamp, 10)
-		fmt.Printf("%d(%d)\t", bot.ObjInfo[i].Data.Price, bot.ObjInfo[i].Volume)
-		fmt.Printf("%d\n", bot.ObjInfo[i].Curve)
+		fmt.Printf("\t%d(%d)", bot.ObjInfo[i].Data.Price, bot.ObjInfo[i].Volume)
+		fmt.Printf("\t%d", bot.ObjInfo[i].Curve)
+		fmt.Println()
 	}
 }
 
@@ -35,8 +36,9 @@ func (bot *Mark1) PrintAt(index int) {
 	if len(bot.ObjInfo) >= index {
 		//fmt.Printf("%s\t", bot.ObjInfo[index].TimeStamp.String())
 		printTimeStamp(bot.ObjInfo[index].TimeStamp, 10)
-		fmt.Printf("%d(%d)\t", bot.ObjInfo[index].Data.Price, bot.ObjInfo[index].Volume)
-		fmt.Printf("%d\n", bot.ObjInfo[index].Curve)
+		fmt.Printf("\t%d(%d)", bot.ObjInfo[index].Data.Price, bot.ObjInfo[index].Volume)
+		fmt.Printf("\t%d", bot.ObjInfo[index].Curve)
+		fmt.Println()
 	}
 }
 
